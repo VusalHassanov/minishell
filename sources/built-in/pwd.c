55faab87d57ab4016907851827f8b3e2dc606a96
Vusal <hassanovvusal@gmail.com>
2025-12-14 21:22:11 +0100

    Added tilde and fixed directory deletion behaviour in cd

--- sources/built-in/pwd.c
+++ sources/built-in/pwd.c
@@ -3,22 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: martin <martin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vhasanov <vhasanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 12:18:47 by vhasanov          #+#    #+#             */
-/*   Updated: 2025/11/24 22:49:45 by martin           ###   ########.fr       */
+/*   Updated: 2025/12/14 21:02:15 by vhasanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_pwd(void)
+int ft_pwd(void)
 {
 	char *cwd;
 
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
+	{
+		perror("pwd");
 		return (1);
+	}
 	printf("%s\n", cwd);
 	free(cwd);
 	return (0);
